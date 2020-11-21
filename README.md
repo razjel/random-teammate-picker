5ab315d78e1bf69857658b58ef9cac61a621eec2
Szczepan Czaicki <s.czaicki@getrprintbox.com>
2020-11-21 01:08:19 +0100

    init config

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-init
+# How to build
+
+for development use:
+
+```
+npm run build
+```               
+
+for building release version and copying it to pbx-js use
+ 
+```
+npm run release
+npm run deploy   
+```
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/Main.ts',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	target: "node",
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	optimization: {
+		minimize: false,
+	},
+};
