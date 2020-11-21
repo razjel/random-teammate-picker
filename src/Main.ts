1f4fba50a5217710520122a28aa507f34213fcde
Szczepan Czaicki <s.czaicki@getrprintbox.com>
2020-11-21 01:05:52 +0100

    init config

---
+++ src/Main.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Szczepan Czaicki, s.czaicki@getprintbox.com
+ * Date: 2020-11-21
+ *
+ * Copyright (c) 2015, Printbox www.getprintbox.com
+ * All rights reserved.
+ */
+export class Main {
+
+}
